c21e0a22682fc71fea1670a7f8630f765ef26324
Utkarsh <83659045+0xShuk@users.noreply.github.com>
2024-10-11 19:09:38 +0530

    remove test file

--- tests/fosho-program.ts
+++ tests/fosho-program.ts
@@ -1,16 +1,16 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { FoshoProgram } from "../target/types/fosho_program";
+// import * as anchor from "@coral-xyz/anchor";
+// import { Program } from "@coral-xyz/anchor";
+// import { FoshoProgram } from "../target/types/fosho_program";
 
-describe("fosho-program", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+// describe("fosho-program", () => {
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.FoshoProgram as Program<FoshoProgram>;
+//   const program = anchor.workspace.FoshoProgram as Program<FoshoProgram>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+//   it("Is initialized!", async () => {
+//     // Add your test here.
+//     const tx = await program.methods.initialize().rpc();
+//     console.log("Your transaction signature", tx);
+//   });
+// });
