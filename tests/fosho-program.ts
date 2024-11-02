92b6923d5ed5f1f5e7f911df9bbea0509ab5400e
crypt0miester <crypt0miester@gmail.com>
2024-11-02 18:24:03 +0400

    fix format

--- tests/fosho-program.ts
+++ tests/fosho-program.ts
@@ -1,91 +1,95 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import {FoshoProgram} from "../target/types/fosho_program";
-import crypto from 'crypto';
+import { FoshoProgram } from "../target/types/fosho_program";
+import crypto from "crypto";
 
-const sleep = (ms: number) => require('timers/promises').setTimeout(ms);
+const sleep = (ms: number) => require("timers/promises").setTimeout(ms);
 
 export function createKnownTestKeypair(knownKey: string) {
   try {
     const deterministicSalt = crypto
-      .createHash('sha256')
+      .createHash("sha256")
       .update(knownKey)
       .digest();
 
-    const newKey = anchor.web3.Keypair.fromSeed(new Uint8Array(deterministicSalt));
-    console.log(knownKey, 'Public Key:', newKey.publicKey.toString());
+    const newKey = anchor.web3.Keypair.fromSeed(
+      new Uint8Array(deterministicSalt)
+    );
+    console.log(knownKey, "Public Key:", newKey.publicKey.toString());
     return newKey;
   } catch (error) {
     throw new Error(`Failed to create keypair: ${error}`);
   }
 }
 
-
 describe("fosho-program", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
   const program = anchor.workspace.FoshoProgram as Program<FoshoProgram>;
-  
+
   const seed = anchor.web3.Keypair.generate().publicKey;
 
-  const [community] = anchor.web3.PublicKey.findProgramAddressSync([
-    Buffer.from("community"),
-    seed.toBuffer()
-  ], 
+  const [community] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("community"), seed.toBuffer()],
     program.programId
   );
 
-  console.log("Community pubkey", community.toString())
+  console.log("Community pubkey", community.toString());
 
   const eventAuthority = createKnownTestKeypair("eventAuthority");
   const eventAttendee1 = createKnownTestKeypair("eventAttendee1");
   const eventCollection = anchor.web3.Keypair.generate();
   const eventAsset1 = anchor.web3.Keypair.generate();
-  console.log("eventCollection pubkey", eventCollection.publicKey.toString())
-  console.log("eventAsset1 pubkey", eventAsset1.publicKey.toString())
+  console.log("eventCollection pubkey", eventCollection.publicKey.toString());
+  console.log("eventAsset1 pubkey", eventAsset1.publicKey.toString());
 
-
-  let mint = new anchor.web3.PublicKey("Db9vq4fNEtaTGUATzibXY2NC7hvFTVPH2w6psYBFyUKn");
-  let ata = new anchor.web3.PublicKey("4cf8Yu7pLDEGzstmQV6Two476dTvb7hC5u8ZtjwcuzX1");
+  let mint = new anchor.web3.PublicKey(
+    "Db9vq4fNEtaTGUATzibXY2NC7hvFTVPH2w6psYBFyUKn"
+  );
+  let ata = new anchor.web3.PublicKey(
+    "4cf8Yu7pLDEGzstmQV6Two476dTvb7hC5u8ZtjwcuzX1"
+  );
 
   const getEvent = (nonce: number) => {
-    const [event] = anchor.web3.PublicKey.findProgramAddressSync([
-      Buffer.from("event"),
-      community.toBuffer(),
-      new anchor.BN(nonce).toArrayLike(Buffer, 'le', 4)
-    ], 
+    const [event] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("event"),
+        community.toBuffer(),
+        new anchor.BN(nonce).toArrayLike(Buffer, "le", 4),
+      ],
       program.programId
     );
 
-    return event
-  }
+    return event;
+  };
 
-  const getAttendeeRecord = (event: anchor.web3.PublicKey, 
-    eventAttendee: anchor.web3.PublicKey) => {
-    const [attendeeRecord] = anchor.web3.PublicKey.findProgramAddressSync([
-      Buffer.from("attendee"),
-      event.toBuffer(),
-      eventAttendee.toBuffer(),
-    ], 
+  const getAttendeeRecord = (
+    event: anchor.web3.PublicKey,
+    eventAttendee: anchor.web3.PublicKey
+  ) => {
+    const [attendeeRecord] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("attendee"), event.toBuffer(), eventAttendee.toBuffer()],
       program.programId
     );
 
-    return attendeeRecord
-  }
-
+    return attendeeRecord;
+  };
 
   it("creates community", async () => {
-    const tx = await program.methods.createCommunity(seed, "testCommunity").rpc();
+    const tx = await program.methods
+      .createCommunity(seed, "testCommunity")
+      .rpc();
     console.log("Your transaction signature", tx);
     console.log("Community Seed: ", seed.toString());
   });
 
   it("creates event, joins event, verify attendance", async () => {
-      const event = getEvent(0)
-      const attendeeRecord = getAttendeeRecord(event, eventAttendee1.publicKey)
-      console.log("Provider pubkey", program.provider.publicKey.toString())
-      const timeNow = Date.now() / 1000;
-      const tx = await program.methods.createEvent(
+    const event = getEvent(0);
+    const attendeeRecord = getAttendeeRecord(event, eventAttendee1.publicKey);
+    console.log("Provider pubkey", program.provider.publicKey.toString());
+    const timeNow = Date.now() / 1000;
+    const tx = await program.methods
+      .createEvent(
         "testEvent",
         "https://example.com/nft.json",
         { inPerson: {} },
@@ -109,111 +113,127 @@ describe("fosho-program", () => {
         "testDescription",
         new anchor.BN(0),
         [eventAuthority.publicKey],
-        true,
-      ).accountsPartial({
+        true
+      )
+      .accountsPartial({
         community,
         authority: program.provider.publicKey,
         tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
         eventCollection: eventCollection.publicKey,
-        mplCoreProgram: new anchor.web3.PublicKey("CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d"),
+        mplCoreProgram: new anchor.web3.PublicKey(
+          "CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d"
+        ),
         rewardAccount: null,
         rewardMint: null,
-        senderAccount: null
-      }).signers([eventCollection])
+        senderAccount: null,
+      })
+      .signers([eventCollection])
       .rpc();
-      console.log("Your transaction signature", tx);
-  
-      const eventData = await program.account.event.fetch(event)
-      console.log(eventData)
+    console.log("Your transaction signature", tx);
 
-      console.log("attendeeRecord", attendeeRecord.toString())
-      // sleep for 2 seconds
-      await sleep(2_000)
-      await program.provider.connection.confirmTransaction(
-        await program.provider.connection.requestAirdrop(
-          eventAttendee1.publicKey,
-          10 * anchor.web3.LAMPORTS_PER_SOL
-        ),
-        "confirmed"
-      );
-      await program.provider.connection.confirmTransaction(
-        await program.provider.connection.requestAirdrop(
-          eventAuthority.publicKey,
-          10 * anchor.web3.LAMPORTS_PER_SOL
-        ),
-        "confirmed"
-      );
-      const joinEventIxn = await program.methods.joinEvent().accountsPartial({
+    const eventData = await program.account.event.fetch(event);
+    console.log(eventData);
+
+    console.log("attendeeRecord", attendeeRecord.toString());
+    // sleep for 2 seconds
+    await sleep(2_000);
+    await program.provider.connection.confirmTransaction(
+      await program.provider.connection.requestAirdrop(
+        eventAttendee1.publicKey,
+        10 * anchor.web3.LAMPORTS_PER_SOL
+      ),
+      "confirmed"
+    );
+    await program.provider.connection.confirmTransaction(
+      await program.provider.connection.requestAirdrop(
+        eventAuthority.publicKey,
+        10 * anchor.web3.LAMPORTS_PER_SOL
+      ),
+      "confirmed"
+    );
+    const joinEventIxn = await program.methods
+      .joinEvent()
+      .accountsPartial({
         community,
         event,
         eventAuthority: eventAuthority.publicKey,
         ticket: eventAsset1.publicKey,
         attendee: eventAttendee1.publicKey,
         eventCollection: eventCollection.publicKey,
-        mplCoreProgram: new anchor.web3.PublicKey("CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d"),
+        mplCoreProgram: new anchor.web3.PublicKey(
+          "CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d"
+        ),
       })
       .instruction();
 
-      joinEventIxn.keys = joinEventIxn.keys.map((key) => {
-        if (key.pubkey.equals(eventAuthority.publicKey)) {
-          return {
-            pubkey: key.pubkey,
-            isSigner: true,
-            isWritable: false,
-          };
-        }
-        return key;
-      })
+    joinEventIxn.keys = joinEventIxn.keys.map((key) => {
+      if (key.pubkey.equals(eventAuthority.publicKey)) {
+        return {
+          pubkey: key.pubkey,
+          isSigner: true,
+          isWritable: false,
+        };
+      }
+      return key;
+    });
 
-      const messageV0 = new anchor.web3.TransactionMessage({
-        instructions: [joinEventIxn],
-        payerKey: eventAttendee1.publicKey,
-        recentBlockhash: (await program.provider.connection.getLatestBlockhash()).blockhash
-      }).compileToV0Message([]);
+    const messageV0 = new anchor.web3.TransactionMessage({
+      instructions: [joinEventIxn],
+      payerKey: eventAttendee1.publicKey,
+      recentBlockhash: (await program.provider.connection.getLatestBlockhash())
+        .blockhash,
+    }).compileToV0Message([]);
 
-      const txJoinEvent = new anchor.web3.VersionedTransaction(messageV0);
-      txJoinEvent.sign([eventAsset1, eventAttendee1, eventAuthority]);
+    const txJoinEvent = new anchor.web3.VersionedTransaction(messageV0);
+    txJoinEvent.sign([eventAsset1, eventAttendee1, eventAuthority]);
 
-      await program.provider.connection.confirmTransaction(
-        await program.provider.connection.sendRawTransaction(txJoinEvent.serialize()),
-        "confirmed"
-      );
-      console.log("txn sent")
-      const attendeeData = await program.account.attendee.fetch(attendeeRecord)
-      console.log(attendeeData)
+    await program.provider.connection.confirmTransaction(
+      await program.provider.connection.sendRawTransaction(
+        txJoinEvent.serialize()
+      ),
+      "confirmed"
+    );
+    console.log("txn sent");
+    const attendeeData = await program.account.attendee.fetch(attendeeRecord);
+    console.log(attendeeData);
 
-
-      const verifyAttendanceIxn = await program.methods.verifyAttendee().accountsPartial({
+    const verifyAttendanceIxn = await program.methods
+      .verifyAttendee()
+      .accountsPartial({
         community,
         event,
         eventAuthority: eventAuthority.publicKey,
         ticket: eventAsset1.publicKey,
         owner: eventAttendee1.publicKey,
         eventCollection: eventCollection.publicKey,
-        mplCoreProgram: new anchor.web3.PublicKey("CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d"),
+        mplCoreProgram: new anchor.web3.PublicKey(
+          "CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d"
+        ),
       })
       .instruction();
 
-      const messageV0VerifyAttendance = new anchor.web3.TransactionMessage({
-        instructions: [verifyAttendanceIxn],
-        payerKey: eventAuthority.publicKey,
-        recentBlockhash: (await program.provider.connection.getLatestBlockhash()).blockhash
-      }).compileToV0Message([]);
+    const messageV0VerifyAttendance = new anchor.web3.TransactionMessage({
+      instructions: [verifyAttendanceIxn],
+      payerKey: eventAuthority.publicKey,
+      recentBlockhash: (await program.provider.connection.getLatestBlockhash())
+        .blockhash,
+    }).compileToV0Message([]);
 
-      const txVerifyAttendance = new anchor.web3.VersionedTransaction(messageV0VerifyAttendance);
-      txVerifyAttendance.sign([eventAuthority]);
+    const txVerifyAttendance = new anchor.web3.VersionedTransaction(
+      messageV0VerifyAttendance
+    );
+    txVerifyAttendance.sign([eventAuthority]);
 
-      await program.provider.connection.confirmTransaction(
-        await program.provider.connection.sendRawTransaction(txVerifyAttendance.serialize()),
-        "confirmed"
-      );
-      console.log("txn sent")
-      const attendeeData2 = await program.account.attendee.fetch(attendeeRecord)
-      console.log(attendeeData2)
-
-      
-
-  })
+    await program.provider.connection.confirmTransaction(
+      await program.provider.connection.sendRawTransaction(
+        txVerifyAttendance.serialize()
+      ),
+      "confirmed"
+    );
+    console.log("txn sent");
+    const attendeeData2 = await program.account.attendee.fetch(attendeeRecord);
+    console.log(attendeeData2);
+  });
   // it("creates event with 5 attendees and no reward", async () => {
   //   const nonce = 0
   //   const event = getEvent(0)
